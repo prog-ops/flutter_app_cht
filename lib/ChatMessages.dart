b6a628bb98ea5b3aaba98d4fcbfaca20810986ee
prgrm274 <programmer270487@gmail.com>
2020-02-24 09:24:46 +0700

    Sen, 24 Feb 2020  9.24.41,75

--- lib/ChatMessages.dart
+++ lib/ChatMessages.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class ChatMessages extends StatefulWidget {
   @override
   _ChatMessagesState createState() => _ChatMessagesState();
+
+  final bool isFriend;
+
+  ChatMessages({
+    Key key,
+    this.isFriend,
+  }) : super(key:key);
 }
 
 class _ChatMessagesState extends State<ChatMessages> {
--- lib/ChatView.dart
+++ lib/ChatView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_dapur_grid/ChatMessages.dart';
 
 class ChatView extends StatefulWidget {
   @override
@@ -13,7 +14,9 @@ class _ChatViewState extends State<ChatView> {
         title: Text("Friend's name"),
       ),
       body: ListView(
-
+        children: <Widget>[
+          ChatMessages()
+        ],
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
