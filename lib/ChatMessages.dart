34604171a3f6c843364c89dc215e0f1971403fb4
prgrm274 <programmer270487@gmail.com>
2020-02-24 09:44:23 +0700

    Sen, 24 Feb 2020  9.44.19,62

--- lib/ChatMessages.dart
+++ lib/ChatMessages.dart
@@ -8,13 +8,35 @@ class ChatMessages extends StatefulWidget {
 
   ChatMessages({
     Key key,
-    this.isFriend,
+    this.isFriend: false,
   }) : super(key:key);
 }
 
 class _ChatMessagesState extends State<ChatMessages> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      height: 100,
+      width: double.infinity,
+      color: Colors.grey[300],
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          widget.isFriend ?
+          CircleAvatar(
+            radius: 32,//34
+            backgroundColor: Colors.white,
+          ) : Container(),
+          Expanded(child: Text("chat here")),
+
+          widget.isFriend == false ?
+          CircleAvatar(
+            radius: 32,//34
+            backgroundColor: Colors.white,
+          ) : Container(),
+        ],
+      ),
+    );
   }
 }
