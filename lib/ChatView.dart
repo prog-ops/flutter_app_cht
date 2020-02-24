938318282c394f332873d5971c79b91c75d7a853
prgrm274 <programmer270487@gmail.com>
2020-02-24 20:36:14 +0700

    Sen, 24 Feb 2020 20.36.11,04

--- lib/ChatView.dart
+++ lib/ChatView.dart
@@ -4,8 +4,8 @@ import 'package:flutter_app_dapur_grid/ChatMessages.dart';
 class ChatView extends StatefulWidget {
   ChatView({
     Key key,
-    this.friendName,
-    this.lastMessage
+    this.friendName: "",
+    this.lastMessage: ""
   }) : super(key: key);
 
   @override
