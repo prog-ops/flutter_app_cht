0114d0000ce6fb1261117265f08b244843e5736a
prog-ops <akunbaru14102019@gmail.com>
2020-03-25 21:31:23 +0700

    signin

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_cht/ChatHead.dart';
-import 'package:flutter_app_cht/ChatView.dart';
 import 'package:flutter_app_cht/views/ChatHistory.dart';
 import 'package:flutter_app_cht/views/HomeView.dart';
 import 'package:flutter_app_cht/views/SettingsView.dart';
@@ -17,61 +15,60 @@ class _MyChatAppState extends State<MyChatApp> {
   bool isSignedIn = false;
 
   @override
+  void initState() {
+    // implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return !isSignedIn
-      ? SigninView()
-      : MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My chatapp"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.help),
-              onPressed: () {
-                print("Button pressed");
-              },
+    return MaterialApp(
+      home: !isSignedIn
+          ? SigninView()
+          : Scaffold(
+              appBar: AppBar(
+                title: Text("Chatta"),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.help),
+                    onPressed: () {
+                      print("Button pressed");
+                    },
+                  ),
+                ],
+              ),
+              body: selectedScreen(_currentIndeks),
+              bottomNavigationBar: BottomNavigationBar(
+                currentIndex: _currentIndeks,
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), title: Text("Home")),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.message), title: Text("Chats")),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.settings), title: Text("Settings")),
+                ],
+                onTap: (int indeks) {
+                  print("indeks is " + indeks.toString());
+                  setState(() {
+                    _currentIndeks = indeks;
+                  });
+                },
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  print("FAB pressed");
+                },
+                child: Icon(Icons.add),
+              ),
             ),
-          ],
-        ),
-
-        body: selectedScreen(_currentIndeks),
-
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndeks,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home), title: Text("Home")
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.verified_user), title: Text("Chats")
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings), title: Text("Settings")
-            ),
-          ],
-          onTap: (int indeks) {
-            print("indeks is " +indeks.toString());
-            setState(() {
-              _currentIndeks = indeks;
-            });
-          },
-        ),
-
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            print("FAB pressed");
-
-          },
-          child: Icon(Icons.add),
-        ),
-      ),
 
 //      home: ChatView(),
     );
   }
 
-  Widget selectedScreen(int _indeks){
-    switch(_indeks) {
+  Widget selectedScreen(int _indeks) {
+    switch (_indeks) {
       case 0:
         return HomeView();
         break;
--- lib/views/SigninView.dart
+++ lib/views/SigninView.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SigninView extends StatefulWidget {
@@ -8,8 +9,40 @@ class SigninView extends StatefulWidget {
 class _SigninViewState extends State<SigninView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Material(
+      child: SafeArea(child: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Center(
+          child: Card(
+            color: Colors.blue[100],
+            margin: EdgeInsets.all(16.0),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Sign in to Chatta',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(height: 40.0,),
+                  RaisedButton.icon(
+                      onPressed: (){},
+                      icon: Icon(Icons.person),
+                      label: Text('Sign In with Google')
+                  ),
+                  SizedBox(height: 40.0,),
+                  Text(
+                    'By signing in you agree to Terms',
+                    style: Theme.of(context).textTheme.body2,
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      )),
     );
   }
 }
